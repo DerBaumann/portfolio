5e0a5291fe4c405f49210c2166dc83c32807a46c
DerBaumann <natur5210@gmail.com>
2026-07-02 09:28:32 +0200

    Adjust the background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import heroBackground from '$lib/assets/forest-waterfall-md.jpg';
+	import heroBackground from '$lib/assets/gray-foggy-forest-md.jpg';
 	import Hero from '$lib/components/Hero.svelte';
 </script>
 
 <Hero
 	background={heroBackground}
-	title="DerBaumann"
+	title="Der Baumann"
 	subtitle="Bastler und Freigeist mit einer besonderen Faszination für Technik und einem autistischen Perfektionismus"
 />
