157084a9985bcdcb7b4a91e5f0133af1469c375b
DerBaumann <natur5210@gmail.com>
2026-07-02 10:06:11 +0200

    Add lucide icons. Add some missing commas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import heroBackground from '$lib/assets/gray-foggy-forest-md.jpg';
 	import aboutImg from '$lib/assets/forest-path-green-md.jpg';
 	import Hero from '$lib/components/Hero.svelte';
+	import { MoveRight } from '@lucide/svelte';
 </script>
 
 <Hero
@@ -16,12 +17,12 @@
 		<div class="space-y-8">
 			<h2 class="h2">Über mich</h2>
 			<p>
-				Ich bin ein Softwareentwickler in Ausbildung im dritten Lehrjahr. Meine Hauptgebiete in
-				denen ich mich bewege sind die Webentwicklung und die Entwicklung von kleinen CLI-Tools, um
+				Ich bin ein Softwareentwickler in Ausbildung im dritten Lehrjahr. Meine Hauptgebiete, in
+				denen ich mich bewege, sind die Webentwicklung und die Entwicklung von kleinen CLI-Tools, um
 				mir den Alltag einfacher zu machen. Ich bin ausserdem ein riesiger Linux-Nerd und kann mich
 				daher auch gut auf Linux-Systemen navigieren.
 			</p>
-			<a href={resolve('/about')} class="btn preset-outlined-primary-500">Mehr</a>
+			<a href={resolve('/about')} class="btn preset-outlined-primary-500">Mehr <MoveRight /></a>
 		</div>
 		<div>
 			<img src={aboutImg} alt="Bild von einem Weg in einem nebligen Wald" />
