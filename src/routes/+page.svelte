af27dd620051c0f0031b26cfec6e6b5dc1cfec38
DerBaumann <natur5210@gmail.com>
2026-07-02 17:16:19 +0200

    Add Projects section. Add featured repo array.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,42 @@
 	import heroBackground from '$lib/assets/gray-foggy-forest-md.jpg';
 	import aboutImg from '$lib/assets/forest-path-green-md.jpg';
 	import Hero from '$lib/components/Hero.svelte';
-	import { MoveRight } from '@lucide/svelte';
+	import { MoveRight, Star } from '@lucide/svelte';
+
+	interface Project {
+		name: string;
+		description?: string;
+		stars: number;
+		link: string;
+	}
+
+	const featuresRepos = $state<Project[]>([
+		{
+			name: 'timelog-rs',
+			description: 'A simple rust cli to track your working time. A rewrite of DerBaumann/timelog',
+			stars: 0,
+			link: 'https://github.com/DerBaumann/timelog-rs'
+		},
+		{
+			name: 'ajour',
+			description:
+				'A better Todo / Task-Management app with an automatic daily todo-list. Written in both Sveltekit and Rust + Axum',
+			stars: 0,
+			link: 'https://github.com/DerBaumann/ajour'
+		},
+		{
+			name: 'dotfiles',
+			stars: 0,
+			link: 'https://github.com/DerBaumann/dotfiles'
+		},
+		{
+			name: 'portfolio',
+			description:
+				'My personal portfolio, that is used for job applications and maybe in the future to offer services to customers',
+			stars: 0,
+			link: 'https://github.com/DerBaumann/portfolio'
+		}
+	]);
 </script>
 
 <Hero
@@ -32,6 +67,19 @@
 	<hr class="border-primary-500 border-2 my-16 rounded-full" />
 
 	<section>
-		<h2 class="h2">Projekte</h2>
+		<h2 class="h2 mb-8">Projekte</h2>
+		<div class="flex gap-8 flex-wrap">
+			{#each featuresRepos as repo (repo.link)}
+				<div class="card bg-surface-800 flex flex-col gap-8 p-4">
+					<div class="flex justify-between flex-1 gap-8">
+						<h3 class="h3">{repo.name}</h3>
+						<p>{repo.stars} <Star class="inline" /></p>
+					</div>
+					<p class="flex-1 max-w-64">{repo.description}</p>
+					<!-- eslint-disable-next-line svelte/no-navigation-without-resolve -->
+					<a href={repo.link} target="_blank" class="self-end btn preset-outlined-primary-500">Zum Repo <MoveRight class="inline" /></a>
+				</div>
+			{/each}
+		</div>
 	</section>
 </main>
