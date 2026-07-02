8b099cd874a026d34fc80a5e4dfd99c9e3fb894a
DerBaumann <natur5210@gmail.com>
2026-07-02 10:00:47 +0200

    Add a some layout to about section. Add image to about.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
 	import heroBackground from '$lib/assets/gray-foggy-forest-md.jpg';
+	import aboutImg from '$lib/assets/forest-path-green-md.jpg';
 	import Hero from '$lib/components/Hero.svelte';
 </script>
 
@@ -10,14 +11,20 @@
 	subtitle="Bastler und Freigeist mit einer besonderen Faszination für Technik und einem autistischen Perfektionismus"
 />
 
-<main>
-	<section>
-		<h2 class="h2">Über mich</h2>
-		<p>
-			Ich habe eine breite Palette an Interessen und Hobbys. Ein grosses Ding ist natürlich die
-			Informatik. Ich liebe es zu Programmieren oder einfach an irgendwelchen Systemen, wie
-			beispielsweise meine Linux-Systeme zu basteln.
-		</p>
-    <a href={resolve("/about")} class="btn preset-outlined-primary-500">Mehr</a>
+<main class="max-w-6xl mx-auto px-24 py-12">
+	<section class="flex gap-16">
+		<div class="space-y-8">
+			<h2 class="h2">Über mich</h2>
+			<p>
+				Ich bin ein Softwareentwickler in Ausbildung im dritten Lehrjahr. Meine Hauptgebiete in
+				denen ich mich bewege sind die Webentwicklung und die Entwicklung von kleinen CLI-Tools, um
+				mir den Alltag einfacher zu machen. Ich bin ausserdem ein riesiger Linux-Nerd und kann mich
+				daher auch gut auf Linux-Systemen navigieren.
+			</p>
+			<a href={resolve('/about')} class="btn preset-outlined-primary-500">Mehr</a>
+		</div>
+		<div>
+			<img src={aboutImg} alt="Bild von einem Weg in einem nebligen Wald" />
+		</div>
 	</section>
 </main>
