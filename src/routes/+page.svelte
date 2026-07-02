5aa979619571e68e812e83bcfe171c2ae4c2b25d
DerBaumann <natur5210@gmail.com>
2026-07-02 09:46:35 +0200

    Add about heading and paragraph

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,3 +8,14 @@
 	title="Der Baumann"
 	subtitle="Bastler und Freigeist mit einer besonderen Faszination für Technik und einem autistischen Perfektionismus"
 />
+
+<main>
+	<section>
+		<h2 class="h2">Über mich</h2>
+		<p>
+			Ich habe eine breite Palette an Interessen und Hobbys. Ein grosses Ding ist natürlich die
+			Informatik. Ich liebe es zu Programmieren oder einfach an irgendwelchen Systemen, wie
+			beispielsweise meine Linux-Systeme zu basteln.
+		</p>
+	</section>
+</main>
