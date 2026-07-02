deeef865f1a8a89e7f79eade6ce870ab92b9e6b2
DerBaumann <natur5210@gmail.com>
2026-07-02 09:11:58 +0200

    Add a bunch of different backgrounds. Add a basic hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import heroBackground from "$lib/assets/forest-waterfall-md.jpg"
+</script>
+
+<header style="background-image: url({heroBackground});" class="px-16 py-24 bg-cover bg-center">
+  <h1 class="h1 mb-6 text-primary-500">DerBaumann</h1>
+  <p class="text-xl text-primary-100">Bastler und Freigeist mit einer besonderen Faszination für Technik und einem autistischen Perfektionismus</p>
+</header>
