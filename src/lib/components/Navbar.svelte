82460f212b7926c5ad8000b77a0f63ed12682c5c
DerBaumann <natur5210@gmail.com>
2026-07-03 13:10:37 +0200

    Fix light navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -18,7 +18,7 @@
 	const { routes, currentPath }: Props = $props();
 </script>
 
-<nav class="sticky top-0 justify-between py-4 px-8 grid grid-cols-3 bg-surface-900 shadow-xl">
+<nav class="sticky top-0 justify-between py-4 px-8 grid grid-cols-3 bg-surface-100 dark:bg-surface-900 shadow-xl">
 	<a href={resolve('/')} class="btn preset-tonal inline w-fit">Der Baumann</a>
 	<ul class="inline-flex justify-center gap-16">
 		{#each routes as route (route.path)}
