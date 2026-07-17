60ccdb07ddb895415dada75abf1c688c7b457a36
DerBaumann <natur5210@gmail.com>
2026-07-17 09:55:41 +0200

    Add about content

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,92 @@
-<h1 class="h1">Über mich</h1>
+<main class="max-w-6xl mx-auto px-24 py-12 space-y-8">
+	<h1 class="h1">Über mich</h1>
 
-<!-- TODO: Read markdown and display it or enter content directly -->
+	<section>
+		<h2 class="h2">Was mache ich und wo helfe ich?</h2>
+		<p>
+			Ich bin ein Softwareentwickler in Ausbildung im dritten Lehrjahr mit dem Ziel, irgendwann
+			selbstständig zu werden. Meine Hauptgebiete, in denen ich mich bewege, sind die Webentwicklung
+			und die Entwicklung von kleinen CLI-Tools, um mir den Alltag einfacher zu machen. Ich bin
+			ausserdem ein riesiger Linux-Nerd und kann mich daher auch gut auf Linux-Systemen navigieren.
+		</p>
+	</section>
+
+	<section>
+		<h2 class="h2">Was sind meine Top Skills / Spezialisierungen</h2>
+		<p>
+			Ich habe mittlerweile einen relativ guten Umgang mit Sveltekit. Mein präferiertes Gebiet ist
+			hier noch die Backend-Entwicklung. Ich kann auch mit Frontend-Arbeit umgehen, würde mich
+			allerdings nicht als Designer betiteln. Ausserdem habe ich auch mit Go etwas rumgebastelt, bin
+			aber aktuell auch dran, mir selbst Rust beizubringen, da ich das Programmiermodell und die
+			Sicherheit sehr schätze. Ich habe auch Erfahrung mit Linux und und etwas in Docker, da ich
+			alles auf Linux (meistens Arch Linux) mache und auch Docker lieben gelernt habe. Ich nutze
+			aktuell Docker noch primär für Testdatenbanken, habe aber auch in der Schule eine komplette
+			App mit Sveltekit Frontend und Go Backend Containerisiert und plane, Docker auch für
+			Deployments zu nutzen.
+		</p>
+		<p>
+			Was ich auf jeden Fall noch lernen will, ist NixOS, falls ich später in den Genuss komme,
+			eigene Serverumgebungen zu pflegen oder keine Lust mehr auf Arch habe. Ausserdem möchte ich
+			mal versuchen, ein eigenes Game in Godot und vielleicht später in Rust mit Bevy zu entwickeln.
+			Dazu besteht weiterhin auch ein Interesse, mich mit funktionalen Sprachen weiter zu befassen.
+			Ich habe minimale Erfahrung in Scala und Gleam, da diese mein Einstieg waren und hätte nichts
+			dagegen, tiefer in diese Sprachen zu gehen oder andere Technologien wie das Phoenix Framework
+			oder Godot für die Game-Entwicklung zu erkunden.
+		</p>
+	</section>
+
+	<section>
+		<h2 class="h2">Was ist mein professioneller Hintergrund</h2>
+		<p>
+			Ich hatte schon früh mit Computer zu tun. Als ich um die 5 Jahre alt war, konnte ich das erste
+			Mal einen Computer nutzen, worauf noch Windows XP lief. Als ich dann 12 war, entdeckte ich
+			Minecraft in der Version 1.7.10 kurz vor dem 1.8 Release. Dort verliebte ich mich ziemlich
+			schnell in das Modding von Minecraft und versuchte meine eigenen Modpacks über die Jahre zu
+			erstellen.
+		</p>
+		<p>
+			Mit 18 konnte ich dann mein erstes IT-Praktikum besuchen, wobei ich eine gute Basis mit HTML,
+			CSS und Java aufbauen konnte und später auch mit Python die Grundlagen vom Programmieren
+			festigen konnte. Ich fing danach irgendwann nach gefühlt 100 IT-Bewerbungen ohne ein
+			Vorstellungsgespräch eine KV-Lehre als Zwischenlösung an, welche ich aber aufgrund von
+			diversen Ursachen nach dem 1. Semester wieder abgebrochen habe.
+		</p>
+		<p>
+			Danach konnte ich zuerst ein Praktikum und danach eine Lehre bei der Klixa als
+			Softwareentwickler starten, worin ich mittlerweile am Anfang des dritten Lehrjahrs stehe. In
+			dieser Zeit konnte ich diverse Dinge lernen. Einige Beispiele sind die Backend-Entwicklung in
+			php, Projektmanagement, Frontend-Entwicklung in Vue und Mobile-Entwicklung in Flutter. Ich
+			habe da auch weiter privat meine Kenntnisse erweitert mit Dingen, wie einem kleinen Einblick
+			in Cyber-Security, Funktionale Programmierung mit Gleam und Scala, Linux durch Linux Mint und
+			Arch, CLI und Backends in Go und Rust, und die Basics von Docker.
+		</p>
+	</section>
+
+	<section>
+		<h2 class="h2">Was macht mich einzigartig?</h2>
+		<p>
+			Ich würde mich selbst als einen Bastler betiteln. Ich liebe es einfach, irgendwelche Dinge
+			auszuprobieren und daran zu basteln, bis ich mein gewünschtes Ergebnis habe. Ich habe
+			ausserdem keine Angst vor komplexeren Sprachen und Tools, solange die Komplexität auch für
+			mich Sinn ergibt.
+		</p>
+	</section>
+
+	<section>
+		<h2 class="h2">Persönliche Interessen / Fun Facts</h2>
+		<p>
+			Eines meiner Lieblingshobbys ist das Kochen, da es einem eine riesige Freiheit bereitet, da
+			man seine Gerichte genau so zubereiten kann, wie man es mag. Es gibt einem auch eine grosse
+			Autonomie, da man nicht an irgendwelche Take-away's, Restaurants oder Fertigmenüs gebunden
+			ist. Ich plane auch in Zukunft noch meinen Garten einzurichten, um mehr Zutaten zur Verfügung
+			zu haben und damit auch noch mehr Geld zu sparen.
+		</p>
+		<p>
+			Was mittlerweile auch ein Teil meines Lebens wurde ist Ernährung und Fitness. Ich habe die
+			Mehrheit meines Lebens vor dem Computer verbracht, Sport massiv vernachlässigt und
+			mittlerweile angefangen, die Quittung dafür zu bekommen. Ich habe mir danach ein Fahrrad
+			besorgt und mit Fitness angefangen. Ich bin immernoch dran, ein System für mich zu finden,
+			aber ich merke jetzt schon Verbesserungen in meiner psychischen und physischen Gesundheit.
+		</p>
+	</section>
+</main>
